2b37a69f9a4b9d2b62ffc232fbec32a14fe7ff66
stealthresponse <stealthresponse+github@gmail.com>
2025-07-21 23:03:34 -0400

    Minor update 1: beginning work on sprites

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from pygame.time import Clock
 from constants import *
 
 def main():
@@ -11,11 +12,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        screen.fill((0,0,0))
+        dt = game_clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
     screen.display.flip()
 
 if __name__ == "__main__":
