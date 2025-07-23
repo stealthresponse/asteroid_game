89f733a7b504ade11007088a613ce8ea3fab9539
stealthresponse <stealthresponse+github@gmail.com>
2025-07-22 22:52:13 -0400

    Minor update 2: continuing work on sprites

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # throughout this file
 import pygame
 from pygame.time import Clock
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
+from player import Player
 
 def main():
     pygame.init()
@@ -14,16 +15,17 @@ def main():
 
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
-        screen.fill((0,0,0))
-        dt = game_clock.tick(60) / 1000
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    screen.display.flip()
+        player.update(dt)
+        screen.fill((0,0,0))
+        player.draw(screen)
+        pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
