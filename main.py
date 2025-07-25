071efebcb50d6ccce1c9175a7c2562de6f4a7784
stealthresponse <stealthresponse+github@gmail.com>
2025-07-24 21:33:43 -0400

    Minor update 3: finished sprite groups and objects. Game is functional but empty.

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ def main():
 
     game_clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
